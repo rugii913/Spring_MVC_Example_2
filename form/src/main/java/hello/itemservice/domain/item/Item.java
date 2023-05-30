ca99b5c9afeb4ebce77ece8dd02ae8f3fa5b37c5
rugii913 <rugii913@gmail.com>
2023-05-30 13:33:14 +0900

    셀렉트 박스
    ㅁ셀렉트 박스에서 띄울 데이터로 자바 객체를 이용해봄
    
    ㅁ역시 th:field에 있는 데이터와 th:value에 있는 데이터를 비교해서
    selected="selected"를 타임리프가 알아서 넣어줌 / 직접 코드 짜면
    지저분해짐

--- form/src/main/java/hello/itemservice/domain/item/Item.java
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -15,7 +15,7 @@ public class Item {
     private Boolean open; //판매 여부
     private List<String> regions; //등록 지역
     private ItemType itemType; //상품 종류 - enum
-    private String deliveryCode; //배송 방식 - String
+    private String deliveryCode; //배송 방식 - String // 하나만 선택하는 것이므로 List가 될 필요가 없다.
     
     public Item() {
     }
