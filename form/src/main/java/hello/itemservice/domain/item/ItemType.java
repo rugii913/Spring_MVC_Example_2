b363a227b6379b7a4fdb6825a5ae66639e70ce70
rugii913 <rugii913@gmail.com>
2023-05-30 12:57:19 +0900

    라디오 버튼
    ㅁ체크박스와 달리 라디오 버튼은 하나만 선택 가능 <input type="radio" ...>
    
    ㅁenum을 사용해봄
     ㅇenum의 values() 메서드를 사용하면 해당 enum의 모든 정보를 배열로
     반환함 -> 앞 다중 체크박스에서 했던 것과 같이 @ModelAttribute로 데이터
     넘길 수 있음
     ㅇenum의 name() 메서드를 타임리프에서 사용 -> enum의 name을 가져온다.
     ex. BOOK을 그대로 String으로 반환
    
    ㅁ라디오 버튼의 경우 값이 없으면 그대로 null을 넘김
     ㅇ체크박스와 달리 라디오 버튼은 별도의 히든 필드가 필요 없다.
     ㅇ이미 하나가 선택이 되어있으면, 수정 시에도 항상 하나를 선택하도록
     되어있기 때문
    
    ㅁ타임리프에서 enum 직접 사용하기(@ModelAttribute로 데이터 넘기지 않고)
     ㅇex. <div th:each="type :
     ${T(hello.itemservice.domain.item.ItemType).values()}">
     ㅇ하지만 이렇게 사용하면 enum의 패키지 변경 등 수정 사항 발생 시 자바 컴파일러가 타임리프 컴파일 오류 잡아내지를 못하므로 권장하지 않음

--- form/src/main/java/hello/itemservice/domain/item/ItemType.java
+++ form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,5 +1,7 @@
 package hello.itemservice.domain.item;
 
+import lombok.Getter;
+
 public enum ItemType {
 
     BOOK("도서"), FOOD("음식"), ETC("기타");
@@ -8,5 +10,9 @@ public enum ItemType {
 
     ItemType(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
