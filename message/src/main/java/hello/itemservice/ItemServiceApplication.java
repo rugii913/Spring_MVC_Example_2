e1ac01d19ee528edf93daa67737238e1f51f2157
rugii913 <rugii913@gmail.com>
2023-05-30 20:10:17 +0900

    메시지 소스 설정, 테스트
    ㅁ스프링 부트의 경우, 스프링부트가 MessageSource를 자동으로 스프링
    빈으로 등록함
     ㅇ설정 필요한 경우 application.properties에서 설정하면 됨(Spring Boot
     Reference Documentation 참고)
     * 파일명이 message가 아니라 messages 임에 유의
    
    ㅁLocale 관련 테스트 작성

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,4 +10,11 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	/*@Bean //스프링부트 사용하면, MessageSource를 자동으로 스프링 빈으로 등록함
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}*/
 }
