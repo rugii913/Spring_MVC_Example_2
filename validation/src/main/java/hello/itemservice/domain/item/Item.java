cda7af7b1144da9aed661949d858d3c79d0f04f2
rugii913 <rugii913@gmail.com>
2023-06-05 14:23:14 +0900

    검증 Form 전송 객체 분리 준비, 장단점 소개
    ㅁBeanValidation의 groups를 잘 사용하지 않는 이유
     ㅇ등록, 수d 시 폼에서 전달하는 데이터가 Item 도메인 객체와 딱 맞지 않기
     때문
     ㅇex. 실무에서는 회원 등록 시 회원 관련 데이터 + 약관 정보 등 도메인
     자체와 관련 없는 수 많은 부가 데이터가 넘어온다.
    
    ㅁ그래서 폼 데이터 전달을 위한 별도의 객체를 사용함
     ㅇ장점: 전송하는 폼 데이터가 복잡해서 별도의 폼 객체를 사용해서
     데이터를 전달 받을 수 있고, 검증이 중복되지 않는다.
     ㅇ단점: 변환 과정이 추가 된다.
    
    * 템플릿 관련 - 등록 수정용 뷰 템플릿이 비슷하면 합치는 게 나은가?
     - 어설프게 합치면 수 많은 if문 때문에 유지보수 어려움
     - 일단 합쳐놨더라도 어설픈 분기문이 보이는 것은 분리해야할 신호임

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,18 +11,18 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) //수정 시에만 적용
+//    @NotNull(groups = UpdateCheck.class) //수정 시에만 적용
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백은 입력할 수 없습니다.")
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백은 입력할 수 없습니다.")
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) //등록 시에만 적용 //그룹에 하나만 있어도 배열로 넘길 수도 있다.
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class}) //등록 시에만 적용 //그룹에 하나만 있어도 배열로 넘길 수도 있다.
     private Integer quantity;
 
     public Item() {
