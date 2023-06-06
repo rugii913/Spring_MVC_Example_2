95b37669ca735b675e651fbb134d7e40f76de482
rugii913 <rugii913@gmail.com>
2023-06-06 15:43:27 +0900

    로그인 처리1 - 쿠키, 세션 - 회원 가입
    ㅁMember, MemberRepository, MemberController 클래스 작성
     ㅇ뷰 템플릿에서 Member 직접 사용하지 않고 별도 객체 두는 게 낫지만,
     일단 그게 핵심이 아니라서 그대로 사용함
     ㅇ리포지토리에서 findByLoginId() 메서드 - null이 나올 수 있기 때문에
     Optional로 감싸서 반환
     ㅇfindByLoginId() 메서드에서 람다, 스트림 사용했음
    ㅁ회원가입 뷰 템플릿 작성
    ㅁ회원용 테스트 데이터 TestDataInit 클래스에 추가

---
+++ login/src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,37 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        //("member") 부분 없어도 되는데, 여기서 명시 안 해놓으면 타임리프 템플릿을 IDE가 해석할 때 인식이 잘 안 돼서 남겨놓음 
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/addMemberForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+}
