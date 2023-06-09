175abc433dad8073b89c467638348d0e580263dc
rugii913 <rugii913@gmail.com>
2023-06-09 17:31:53 +0900

    로그인 처리 2 - 서블릿 필터 - 인증 체크 적용
    ㅁLoginCheckFilter - 인증 체크 필터 클래스 추가
     ** Filter 구현할 때 나머지는 빼고 doFilter()만 구현해도 됨
     ㅇwhitelist 배열 및 whitelist 확인 메서드 추가
      - 확인 메서드에서 스프링 제공 PatternMatchUtils 사용함
    
     ㅇ미인증 사용자인 경우 httpResponse.sendRedirect(~) 이용하여 리다이렉트
      - requestURI 저장해 놓고 로그인 페이지로 연결시킬 때 남겨놓는데
     ***** return;으로 미인증 사용자는 다음으로 진행하지 않고 끝
    
    ㅁWebConfig 클래스에 loginCheckFilter 추가 및 빈 등록
     ㅇaddUrlPatterns("/*") - 필터 내에 whitelist 넣어놨으므로, 모든 요청에
     필터 적용함 / 만약 addUrlPatterns로 하나씩 추가하면 새로운 url 생길
     때마다 추가해줘야 해서 불편함
    
    *** 필터 하나 더 호출하는 것으로 인한 성능 문제는 고려하지 않아도 된다.
    대부분의 성능문제는 1.DB쿼리, 2.외부 네트워크 필요한 것 때문에 발생한다.
    
    ㅁLoginController - loginV4() 메서드
     ㅇredirectURL 남겨놓은 것 사용

--- login/src/main/java/hello/login/web/filter/LogFilter.java
+++ login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -17,6 +17,8 @@ public class LogFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
 
