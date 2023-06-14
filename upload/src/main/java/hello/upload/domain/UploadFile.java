6006636840d52f082c6098a82cce2ed9976daa77
rugii913 <rugii913@gmail.com>
2023-06-14 16:28:40 +0900

    파일 업로드, 다운로드 예제(2)
    ㅁFileStore 클래스 - 파일 저장과 관련된 업무 처리
     ㅇfileDir 필드 - 파일 저장할 경로 application.properties 속성
    
     ㅇgetFullPath() - fileDir와 뒤에서 정할 filename 연결해서 FullPath String
     만들어주는 메서드
    
     ㅇextractExt() - 올라온 파일 이름에서 확장자만 뽑아냄
     ㅇcreateStoreFileName() - UUID로 저장할 파일 이름 랜덤으로 생성해서
     원래 확장자랑 붙여줌
    
     ㅇstoreFile()
      - createStoreFileName으로 만든 파일 이름을 이용, getFullPath로 전체
        경로 String으로 뽑아내고
      - multipartFile.transferTo로 해당 경로에 파일 저장
      - UploadFile 객체 반환
    
     ㅇstoreFiles()
      - 파일이 여러 개 들어올 수 있으므로 UploadFile의 리스트로

--- upload/src/main/java/hello/upload/domain/UploadFile.java
+++ upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -4,4 +4,9 @@ public class UploadFile {
 
     private String uploadFileName;
     private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
 }
